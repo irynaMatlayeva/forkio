92260d5d35626ea8f812323cda85986ba2ce791b
irynaMatlayeva <i.matlayeva@gmail.com>
2020-12-11 14:49:08 +0200

    add Dropdown Open and Close function

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,29 @@
 "use strict";
 
-let burger = document.querySelector(`.burger`);
-let menu = document.querySelector(`.menu__list`);
-burger.addEventListener("click", function () {
-    menu.classList.toggle("active");
-    burger.classList.toggle("open");
-});
+function dropDown() {
+    let burger = document.querySelector(`.burger`);
+    let menu = document.querySelector(`.menu__list`);
+
+    const openMenu = () => {
+        menu.classList.toggle("active");
+        burger.classList.toggle("open");
+    };
+
+    burger.addEventListener('click', (e) => {
+        e.stopPropagation();
+        openMenu();
+    });
+
+    document.addEventListener("click", (e) => {
+        const target = e.target;
+        const isMenuTarget = target == menu || menu.contains(target);
+        const isMenuActive = menu.classList.contains("active");
+
+        if (!isMenuTarget && isMenuActive) {
+            openMenu();
+        }
+    });
+}
+
+dropDown();
+
